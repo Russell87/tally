72e14f981715709950bfaf05b293ce6e8203cb91
Russell <russellchristensen@me.com>
2015-07-19 22:12:18 +0000

    Fixed goals migration errors

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150719042505) do
+ActiveRecord::Schema.define(version: 20150719215146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "goals", force: :cascade do |t|
     t.string   "name"
-    t.string   "description"
+    t.text     "description"
     t.boolean  "completed"
+    t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
+  add_index "goals", ["user_id", "created_at"], name: "index_goals_on_user_id_and_created_at", using: :btree
+  add_index "goals", ["user_id"], name: "index_goals_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -33,4 +37,5 @@ ActiveRecord::Schema.define(version: 20150719042505) do
     t.string   "remember_digest"
   end
 
+  add_foreign_key "goals", "users"
 end
