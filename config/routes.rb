6534070f82be73f1a82c6e0864619f935fe518af
Russell <russellchristensen@me.com>
2015-07-19 00:11:31 +0000

    Working login function

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   resources :users
-  
   get 'users/new'
-
   root  'pages#index' 
-  get 'about'     => 'pages#about'
-  get 'contact'   => 'pages#contact'
-  get 'signup'    => 'users#new'
+  get 'about'      => 'pages#about'
+  get 'contact'    => 'pages#contact'
+  get 'signup'     => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
